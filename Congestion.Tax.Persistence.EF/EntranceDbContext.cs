b42094757db66c5f5f1e1a7b3932142edfa237a3
mrilia <mrilia@DEV-PC>
2023-07-12 20:55:22 +0330

    feat: add in-memory database

---
+++ Congestion.Tax.Persistence.EF/EntranceDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Congestion.Tax.Business.Models.Entrance;
+using Microsoft.EntityFrameworkCore;
+
+namespace Congestion.Tax.Persistence.EF
+{
+    public class TaxDbContext : DbContext
+    {
+        public TaxDbContext(DbContextOptions<TaxDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Entrance> Entrances { get; set; }
+
+        public Entrance AddInterance(Entrance entrance)
+        {
+            Entrances.Add(entrance); 
+            return entrance;
+        }
+
+        public IQueryable<Entrance> GetAllEntrances()
+        {
+            return Entrances.AsQueryable();
+        }
+    }
+}
---
+++ Congestion.Tax.Persistence.EF/EntranceRepository.cs
@@ -0,0 +1,28 @@
+﻿using Congestion.Tax.Business;
+using Congestion.Tax.Business.Models.Entrance;
+using Microsoft.EntityFrameworkCore;
+
+namespace Congestion.Tax.Persistence.EF
+{
+    public class EntranceRepository : IEntranceRepository
+    {
+        private readonly TaxDbContext _dbContext;
+
+        public EntranceRepository(TaxDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Entrance>> GetAllAsync()
+        {
+            return await _dbContext.Entrances.ToListAsync();
+        }
+
+        public async Task<Entrance> AddAsync(Entrance entrance)
+        {
+            _dbContext.Entrances.Add(entrance);
+            await _dbContext.SaveChangesAsync();
+            return entrance;
+        }
+    }
+}
